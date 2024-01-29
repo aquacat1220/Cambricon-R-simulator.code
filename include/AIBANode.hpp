b84748fea28ee5d1fa5551d937922d257677f6d0
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2024-01-29 06:50:23 +0000

    feat: Add skeleton code for AIBANode

--- include/AIBANode.hpp
+++ include/AIBANode.hpp
@@ -1,12 +1,36 @@
 #ifndef AIBA_NODE
 #define AIBA_NODE
 
+#include <vector>
 
+#include "AIBACommon.hpp"
+
+using namespace std;
 
 class AIBANode {
     public:
+    AIBANode();
+
+    public:
+    // Inputs
+    vector<Request> in_left_reqs;
+    vector<Packet> in_up_pkts, in_down_pkts, in_left_pkts, in_right_pkts;
+    vector<Sum> in_up_sums;
+    // Outputs
+    vector<Request> out_right_reqs;
+    vector<Packet> out_up_pkts, out_down_pkts, out_left_pkts, out_right_pkts;
+    vector<Sum> out_down_sums;
+
+    public:
     void Cycle();
     void HashTableLoad(int* hash_table);
+
+    private:
+    vector<Request> local_request_buffer;
+    vector<PSum> psum_buffer;
+    vector<int> sram_bank;
+    // Coordinate in NoC of AIBA nodes. Range between 0 ~ 255.
+    unsigned char coord;
 };
 
 #endif
