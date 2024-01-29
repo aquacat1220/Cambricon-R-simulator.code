5593d296ffd01f2384bffcf09c30ad24a240b9af
TR2-K <treek00@naver.com>
2024-01-29 07:42:14 +0000

    feat: Write sekeleton code for AIBANode::Cycle()

--- src/AIBANode.cpp
+++ src/AIBANode.cpp
@@ -4,7 +4,29 @@ AIBANode::AIBANode(unsigned char coord) {
     this->coord = coord;
 }
 
-void AIBANode::Cycle() {
+void AIBANode::Cycle(void) {
+    //Requests
+    //addr comparison -> LRB or NRB
+    //while(in_left_reqs.)
+    //if(in_left_reqs[0].addr)
+        //case1. LRB
+            //
+
+        //case2. NRB 
+
+    //Packets
+    //Routing logic
+
+        //case1. transfer Packet to target node
+    vector<Packet> arrival_buffer;
+        //case2. destination node
+            //Psum computing
+            //Psum counter check (==7)
+
+    //Sum
+    //transfer to bottom node
+
+
     return;
 }
 
