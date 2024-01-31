02a139e515234688398b60fd00417aa8c5dd163b
aquacat1220 <155734094+aquacat1220@users.noreply.github.com>
2024-01-31 06:28:26 +0000

    feat: Implement AIBA::AIBA

--- src/AIBA.cpp
+++ src/AIBA.cpp
@@ -1,7 +1,16 @@
 #include "AIBA.hpp"
 
-AIBA::AIBA(int grid_resolution) : AIBAPP_(grid_resolution) {
+#include <iostream>
 
+AIBA::AIBA(int grid_resolution) : AIBAPP_(grid_resolution) {
+    // Initialize AIBANode mesh.
+    this->AIBANodes_.reserve(16 * 16);
+    for (unsigned int coord = 0; coord < 256; coord++) {
+        AIBANodes_.emplace_back(static_cast<unsigned char>(coord));
+    }
+
+    if (AIBANodes_.size() != 256)
+        cout << ("[ERROR] AIBA::AIBANode_.size() is not 256.") << endl;
 }
 
 void AIBA::Cycle(){
