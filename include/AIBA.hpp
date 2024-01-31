d7780896d52966bb53305256fd50e2ffe9ef3821
TR2-K <treek00@naver.com>
2024-01-31 05:36:40 +0000

    feat: Wrote AIBA public interface

--- include/AIBA.hpp
+++ include/AIBA.hpp
@@ -1,4 +1,20 @@
-#ifndef AIBA
-#define AIBA
+#ifndef AIBA_ARC
+#define AIBA_ARC
 
+#include "AIBAPP.hpp"
+#include "AIBANode.hpp"
+
+class AIBA{
+    private:
+
+    protected:
+        AIBAPP _AIBAPP;
+        vector<AIBANode> _AIBANodes;
+        vector<Sum> _output_buffer;
+
+    public:
+        AIBA();
+        void Cycle(void);
+        void Transition(void);
+};
 #endif
---
+++ src/AIBA.cpp
@@ -0,0 +1,16 @@
+#include "AIBA.hpp"
+
+AIBA::AIBA(){
+}
+
+void AIBA::Cycle(){
+    //_AIBAPP.Cycle()
+    //_AIBANodes
+
+}
+
+void AIBA::Transition(){
+    //address crossbar
+    //output crossbar
+
+}
