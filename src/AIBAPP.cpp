fa924d45dc6c2c9bb6c5e76590a18fee4f912d0d
aquacat1220 <aquacat1220@gmail.com>
2024-02-01 21:58:46 +0000

    feat: Implement AIBAPP::TriLerp(), AIBAPP::Cycle()

--- src/AIBAPP.cpp
+++ src/AIBAPP.cpp
@@ -21,16 +21,14 @@ void AIBAPP::Cycle() {
         grid_y = static_cast<int>(pt.y * this->grid_resolution_);
         grid_z = static_cast<int>(pt.z * this->grid_resolution_);
 
-        // Structured bindings + tuple in C++!! So cool.
         tuple<int, int, int> grid_offsets[8] = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1}, {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
-        for (auto [grid_offx, grid_offy, grid_offz] : grid_offsets) {
-            grid_x = grid_x + grid_offx;
-            grid_y = grid_y + grid_offy;
-            grid_z = grid_z + grid_offz;
-            unsigned int addr = Hash(grid_x, grid_y, grid_z) % (1 << 19);
-            
-            // @todo Implement trilinear interpolation to calculate weight.
-            float weight;
+
+        auto weights = TriLerp((pt.x * this->grid_resolution_) - grid_x, (pt.y * this->grid_resolution_) - grid_y, (pt.z * this->grid_resolution_) - grid_z);
+
+        for (int i = 0; i < 8; i++) {
+            auto [grid_offx, grid_offy, grid_offz] = grid_offsets[i];
+            float weight = weights[i];
+            unsigned int addr = Hash(grid_x + grid_offx, grid_y + grid_offy, grid_z + grid_offz) % (1 << 19);
 
             out_reqs.emplace_back(this->ridx_reg_, pidx, addr, weight, this->dest_reg_ + pidx);
         }
@@ -46,5 +44,12 @@ unsigned int AIBAPP::Hash(int grid_x, int grid_y, int grid_z) {
     return (grid_x * 1) ^ (grid_y * 2654435761) ^ (grid_z * 805459861);
 }
 
-vector<float> TriLerp(float offx, float offy, float offz) {
+vector<float> AIBAPP::TriLerp(float offx, float offy, float offz) {
+    vector<float> return_vec;
+    return_vec.reserve(8);
+    tuple<int, int, int> grid_offsets[8] = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1}, {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
+    for (auto [grid_offx, grid_offy, grid_offz] : grid_offsets) {
+        return_vec.push_back(abs((grid_offx - offx) * (grid_offy - offy) * (grid_offz - offz)));
+    }
+    return return_vec;
 }
