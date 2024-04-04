350cc29ba2317e8ed3fec006879a917e0c49ac49
TR2-K <treek00@naver.com>
2024-04-04 16:46:28 +0000

    fix: Minor fixes in MLPUnit.hpp

--- include/MLPUnit.hpp
+++ include/MLPUnit.hpp
@@ -24,105 +24,105 @@ class MlpUnit {
      * @brief Final pixel color of the corresponding ray
      * 
     */
-   Pixel out_pixel;
+    Pixel out_pixel;
 
-   /**
+    /**
     * @brief ridx of the ray processed by this MLP Unit
-   */
-   unsigned int ridx;
+    */
+    unsigned int ridx;
 
-   /**
+    /**
     * @brief View direction Theta & Phi
     * Projected onto the first 16 coefficients of the spherical harmonics basis
     * Cleared and refilled when computed ray is changed.
-   */
-   float theta;
-   float phi;
+    */
+    float theta;
+    float phi;
 
-   /**
+    /**
     * @brief Dendity threshold value for early termination
     * Suppose that this value is fixed value stored in ROM. 
-   */
-   float threshold;
+    */
+    float threshold;
 
-   public:
-   /**
+    public:
+    /**
     * @brief Compute density and color throuh density network and color network
     * This function should be called every cycle.
     * Computes a single batch untill remain_cycle_=0 .
     * Cycle() doesn't magically propagate the outputs to the global buffer. This is a responsibility of the system.
-   */
-   void Cycle();
-   /**
+    */
+    void Cycle();
+    /**
     * @brief Laods the MLP weights to MLP unit
     * Since we consider only inference, this function called only one time initially by the system  
     * MLP weight consists of several matrices (32x64, 64x16 for density network & 32x64, 64x64, 64x3 for color network)
-   */
-   void MlpWeightLoad(vector<vector<float>>& w1_d, vector<vector<float>>& w2_d, vector<vector<float>>& w1_c, vector<vector<float>>& w2_c, vector<vector<float>>& w3_c);
+    */
+    void MlpWeightLoad(vector<vector<float>>& w1_d, vector<vector<float>>& w2_d, vector<vector<float>>& w1_c, vector<vector<float>>& w2_c, vector<vector<float>>& w3_c);
 
-   private:
-   /**
+    private:
+    /**
     * @brief Remained cycles for a single batch MLP computation
     * If this variable != 0, MLP unit cannot proceed another batch
-   */
-   int remain_cycle_; 
+    */
+    int remain_cycle_; 
 
-   /**
+    /**
     * @brief MLP weights for MLP
     * (32x64, 64x16 for density network & 32x64, 64x64, 64x3 for color network)
-   */
-   vector<vector<float>> w1_density_;
-   vector<vector<float>> w2_density_;
-   vector<vector<float>> w1_color_;
-   vector<vector<float>> w2_color_;
-   vector<vector<float>> w3_color_;
-   
-   /**
+    */
+    vector<vector<float>> w1_density_;
+    vector<vector<float>> w2_density_;
+    vector<vector<float>> w1_color_;
+    vector<vector<float>> w2_color_;
+    vector<vector<float>> w3_color_;
+
+    /**
     * @brief Save the density and color values of the sample points of the corresponding ray.
     * 
-   */
-   vector<float> densities_;
-   vector<vector<float>> colors_;
-   vector<float> distances_;
+    */
+    vector<float> densities_;
+    vector<vector<float>> colors_;
+    vector<float> distances_;
 
-   /**
+    /**
     * @brief For checking whether density over threshold or not
-   */
-   float accumulate_dens_;
+    */
+    float accumulate_dens_;
 
-   private:
-   /**
+    private:
+    /**
     * @brief Clears all inputs
     * 
-   */
-   void ClearInputs();
+    */
+    void ClearInputs();
 
-   /**
+    /**
     * @brief Clears private variables
     * 
-   */
-  void ClearLocals();
+    */
+    void ClearLocals();
 
-   /**
+    /**
     * @brief Compute final color. (Formula in NeRF paper)
     * 
-   */
-   Pixel ComputeColor();
+    */
+    Pixel ComputeColor();
 
-   /**
+    public:
+    /**
     * @brief Get a constant reference to remain_cycle_
     * System uses this function for checking corresponding MLP Unit done current batch computation.
-   */
-   const int& GetRemainCycle();
+    */
+    const int& GetRemainCycle();
 
-   public:
     /**
-     * @brief Set distances_ to meaningless distance values b/w two samples for testing.
-     * Distance value is required for computing color.
-     * However, we don't have coordinate value in Feature structure.
-     * 
-     * @warning Should only be used for testing purposes.
-     */
+    * @brief Set distances_ to meaningless distance values b/w two samples for testing.
+    * Distance value is required for computing color.
+    * However, we don't have coordinate value in Feature structure.
+    * 
+    * @warning Should only be used for testing purposes.
+    */
     void SetDistance();
 };
 
