674f40692f3344b6ad86032b07f41ecc15b1baca
TR2-K <treek00@naver.com>
2024-05-17 08:39:01 +0000

    fix: Fix EmptyCycle test for MLPUnit

--- test/TestMLPUnit/StartEmpty.cpp
+++ test/TestMLPUnit/StartEmpty.cpp
@@ -4,18 +4,34 @@
 #include <algorithm>
 
 int main() {
-    /*
     // Create MLPUnit
-    MlpUnit mlpunit = MlpUnit();
+    MlpUnit mlpunit = MlpUnit(0);
+
+    mlpunit.theta = 0.1f;
+    mlpunit.phi = 0.1f;
+    mlpunit.threshold = 1.0f;
+    mlpunit.SetDistance();
+
+    vector<vector<float>> w1_density(32, vector<float>(64, 1e-4));
+    vector<vector<float>> w2_density(64, vector<float>(16, 1e-4));
+    vector<vector<float>> w1_color(32, vector<float>(64, 1e-4));
+    vector<vector<float>> w2_color(64, vector<float>(64, 1e-4));
+    vector<vector<float>> w3_color(64, vector<float>(3, 1e-4));
+
+    mlpunit.MlpWeightLoad(w1_density, w2_density, w1_color, w2_color, w3_color);
 
     auto before_in = mlpunit.in_features;
-    auto before_out = mlpunit.out_pixel;
 
     sort(before_in.begin(), before_in.end());
 
     assert(before_in == vector<Feature>());
-    assert(!before_out.ridx);
-    */
+
+    for (int i=0; i<1000; ++i) {
+        mlpunit.Cycle();
+        assert(mlpunit.in_features.empty());
+        assert(!mlpunit.GetHasOutput());
+        assert(mlpunit.is_idle());
+    }
 
     return 0;
 }
